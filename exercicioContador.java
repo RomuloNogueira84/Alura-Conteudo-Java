ae33811ae932f289a8e7715d07f12470a72a7bdd
Romulo Nogueira <romulonogueira929@gmail.com>
2024-04-23 09:29:22 -0300

    commit 23-04

--- exercicioContador.java
+++ exercicioContador.java
@@ -6,6 +6,12 @@ public class exercicioContador {
                 while(contador <= 10) {
                     System.out.println(contador);
                     contador++;
+                    if (contador<=10){
+                        System.out.println("Que venha mais um numero agora é o ");
+
+                    }else {
+                        System.out.println("Chegamos ao Dez pedi pra parar parou.");
+                    }
                 }
     }
 }
